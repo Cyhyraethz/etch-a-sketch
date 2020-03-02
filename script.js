a98444f30820e722eeace2dd897542bb064fa0bb
Dylan Premo <maboose@yahoo.com>
2020-03-02 13:04:34 -0800

    Populate grid, change color on mouseover

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let container = document.getElementById('container');
 
 for (let i = 0; i < 256; i++) {
   let div = document.createElement('div');
+  div.setAttribute('onmouseover', 'changeColor(this)');
   div.className = 'item';
   container.appendChild(div);
+}
+
+function changeColor(e) {
+  e.id = 'newColor';
+  setTimeout(function() {
+    e.removeAttribute('id');
+  }, 1000);
 }
