b09a1f497b91a5bb90f01e8522c1ff8e8ed23f2e
Dylan Premo <maboose@yahoo.com>
2020-03-02 17:02:41 -0800

    Add button to reset the grid to a different resolution

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let container = document.getElementById('container');
+const innerContainer = document.getElementById('innerContainer');
+const container = document.getElementById('container');
 
-for (let i = 0; i < 256; i++) {
-  let div = document.createElement('div');
-  div.setAttribute('onmouseover', 'changeColor(this)');
-  div.className = 'item';
-  container.appendChild(div);
+function createGrid() {
+  for (let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
+    div.setAttribute('onmouseover', 'changeColor(this)');
+    div.className = 'item';
+    container.appendChild(div);
+  }
 }
 
 function changeColor(e) {
@@ -13,3 +16,26 @@ function changeColor(e) {
     e.removeAttribute('id');
   }, 1000);
 }
+
+function resetGrid() {
+  let squaresPerSide = prompt(
+    'Please enter the number of squares you would like on each side of your new grid (between 1 and 100)...'
+  );
+  while (squaresPerSide > 100 || squaresPerSide < 1) {
+    squaresPerSide = prompt(
+      'Please enter the number of squares you would like on each side of your new grid (between 1 and 100)...'
+    );
+  }
+  let totalSquares = squaresPerSide * squaresPerSide;
+  container.innerHTML = '';
+  container.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+  for (let i = 0; i < totalSquares; i++) {
+    let div = document.createElement('div');
+    div.setAttribute('onmouseover', 'changeColor(this)');
+    div.className = 'item';
+    container.appendChild(div);
+  }
+}
+
+createGrid();
